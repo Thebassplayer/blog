f06bce7ada28ecb02a12abf41c792c957a296af8
Roy Lopez <ryhsu2@gmail.com>
2024-11-28 19:06:10 +0100

    refactor: update TypeScript configuration and improve server setup
    - Changed tsconfig.json to include files from the current directory
    - Updated package.json for 'posts' and 'comments' to use TypeScript entry points
    - Implemented basic CRUD operations in index.ts for managing posts

--- posts/index.ts
+++ posts/index.ts
@@ -1,3 +1,37 @@
 import express, { Express } from "express";
-
+import { randomBytes } from "crypto";
 const app: Express = express();
+
+type Post = {
+  id: string;
+  title: string;
+};
+
+type ID = string;
+
+type Posts = {
+  [key: ID]: Post;
+};
+
+const posts: Posts = {};
+
+app.use(express.json());
+
+app.get("/post", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/post", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
+});
